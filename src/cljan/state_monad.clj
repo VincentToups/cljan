9b36c2f1b7bf4426ea25b23f63db80a4ed24a557
Eduardo Bellani <ebellani@gmail.com>
2014-09-23 19:54:13 -0300

    Add more types
    Getting my ass handed over to me by `state-get`

--- src/cljan/state_monad.clj
+++ src/cljan/state_monad.clj
@@ -1,19 +1,19 @@
 (ns cljan.state-monad
-  (:require [clojure.core.typed :as t]
+  (:require [clojure.core.typed :refer [ann All HVec U Any Map Seq Coll Option]
+             :as typed]
             [clojure.core.match :refer [match]]))
 
-(t/ann state-return (t/All [X Y] [X -> [Y -> (t/HVec [X Y])]]))
+(t/ann state-return (All [X Y] [X -> [Y -> (HVec [X Y])]]))
 (defn state-return [x]
   (fn [state]
     [x state]))
 
-(t/ann state-bind
-       (t/All [State X Y]
-              [[State -> (t/HVec [X State])]
-               [X -> [State -> (t/HVec [Y State])]]
-               ->
-               [State -> (t/HVec [Y State])]]))
-
+(ann state-bind
+     (All [state x y]
+          [[state -> (HVec [x state])]
+           [x -> [state -> (HVec [y state])]]
+           ->
+           [state -> (HVec [y state])]]))
 (defn state-bind [mv mf]
   (fn [state]
     (let [[value new-state] (mv state)]
@@ -48,104 +48,62 @@
                    ~true-branch
                    ~false-branch))))
 
-(t/ann state-assoc
-       (t/All [k v x]
-              [k v -> [(clojure.lang.IPersistentMap k v) ->
-                       (t/HVec [v (clojure.lang.IPersistentMap k v)])]]))
+(ann state-assoc
+     (All [k v]
+          [k v -> [(Map k v) -> (HVec [v (Map k v)])]]))
 (defn state-assoc [key val]
   (fn [state]
     [val
      (assoc state key val)]))
 
+(ann state-assoc-in
+     (All [v]
+          [(Seq Any) v -> [(Map Any Any) ->
+                           (HVec [v Any])]]))
 (defn state-assoc-in [keys val]
   (fn [state]
     [val (assoc-in state keys val)]))
 
+(ann state-get
+     [Any *
+      ->
+      [(U (Option (clojure.lang.ILookup Any Any)) Any)
+       ->
+       (HVec [(Option Any)
+              (U (Option (clojure.lang.ILookup Any Any)) Any)])]])
+
 (defn state-get [& keys]
   (fn [state]
-    [(loop [val state
-            keys keys]
+    [(typed/loop [val  :- (U (Option (clojure.lang.ILookup Any Any)) Any)
+                  ,  state
+                  keys :- (Option (typed/I (typed/CountRange 1)
+                                           (typed/HSeq (Any *))))
+                  , keys]
        (if (or (empty? keys) (nil? val))
          val
-         (recur (val (first keys))
+         (recur (get val (first keys))
                 (rest keys))))
      state]))
 
-(defn extract-state [state]
-  [state state])
+;; (defn extract-state [state]
+;;   [state state])
 
-(defn set-state [new-state]
-  (fn [old-state]
-    [new-state new-state]))
+;; (defn set-state [new-state]
+;;   (fn [old-state]
+;;     [new-state new-state]))
 
-(defmacro defstatefn [name args & body]
-  `(defn ~name ~args (state-do ~@body)))
+;; (defmacro defstatefn [name args & body]
+;;   `(defn ~name ~args (state-do ~@body)))
 
-(defn state-reduce 
-  "Using F, a state-function which takes an accumulator and an item, reduce
-  the sequence COLLECTION starting with the initial value INIT."
-  [f collection init]
-  (fn [state]
-    (loop [result init
-           state state
-           collection collection]
-      (cond 
-       (empty? collection) [result state]
-       :otherwise 
-       (let [first (first collection)
-             rest (rest collection)
-             [new-result new-state] ((f result first) state)]
-         (recur new-result new-state rest))))))
-
-(defn state-for-each
-  "For each item in collection, call the state-function F on the item."
-  [f collection]
-  (fn [state]
-    (loop [state state
-           collection collection]
-      (cond 
-       (empty? collection) [nil state]
-       :otherwise 
-       (let [first (first collection)
-             rest (rest collection)
-             [ignored new-state] ((f first) state)]
-         (recur new-state rest))))))
-
-(defn state-repeat 
-  "Repeat the parameterized state-function F N times, passing the
-  iteration as the first parameter."
-  [n f]
-  (state-for-each f (range n)))
-
-(defn state-map 
-  "Map F, a state-function, across the sequence COLLECTION and return
-  a COLLECTION of the results."
-  [f collection]
-  (fn [state]
-    (loop [result []
-           state state
-           collection collection]
-      (cond 
-       (empty? collection) [result state]
-       :otherwise 
-       (let [first (first collection)
-             rest (rest collection)
-             [val new-state] ((f first) state)]
-         (recur (conj result val) new-state rest))))))
-
-(defn state-dip 
-  "Given a chain of keys pointing to somewhere in the state, pass that
-  value to f and set the value to the return value of f." 
-  [location f]
-  (state-do 
-   [:bind v (apply state-get location)]
-   (state-assoc-in location (f v))))
-
-(defn state-call 
-  "Given F and a set of values in the state-monad apply F, which
-  should be a function producing a state function, to the unwrapped
-  values implied by the subsequent arguments."
-  [f & rest]
-  (state-do 
-   [:bind unwrapped-valued (state-map identity rest)]
-   (apply f unwrapped-valued)))
+;; (defn state-map [f collection]
+;;   (fn [state]
+;;     (loop [result []
+;;            state state
+;;            collection collection]
+;;       (match [collection]
+;;              [([] :seq)] [result state]
+;;              [([first & rest] :seq)]
+;;              (let [[val state] ((f first) state)]
+;;                (recur (conj result val)
+;;                       state
+;;                       rest))))))
